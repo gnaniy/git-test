6c0cfe0dbc1d7ae77d16e10759327318a5362b20
yinang <gnaniy@163.com>
2021-05-28 18:08:55 +0800

    thrid commit

--- src/main/java/gitDemo.java
+++ src/main/java/gitDemo.java
@@ -2,5 +2,6 @@ public class gitDemo {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("你好，git");
+        System.out.println("gittttttt");
     }
 }
