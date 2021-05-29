ab6c90a5353bcfa329bdfed06657288129d0c331
yinang <gnaniy@163.com>
2021-05-29 14:18:00 +0800

    push commit

--- src/main/java/gitDemo.java
+++ src/main/java/gitDemo.java
@@ -6,5 +6,6 @@ public class gitDemo {
         System.out.println("git4");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
